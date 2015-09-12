678953f9bca8e5d208837f358539698167d85084
Javier Rizzo <javierrizzoa@gmail.com>
2015-09-12 06:10:52 -0700

    Fucking diffs

--- finger-hero.js
+++ finger-hero.js
@@ -1,3 +1,4 @@
+import Leap from 'leapjs';
 var FPS = 30;
 var ctx = document.getElementById('finger-hero-canvas').getContext('2d');
 var squares = [];
@@ -67,4 +68,32 @@ function drawFrets() {
     ctx.lineTo(180 + 70 * i, 480);
     ctx.stroke();
   }
+}
+
+var controllerOptions = {enableGestures: true};
+Leap.loop(controllerOptions, (frame) => {
+  if (frame.hands.length > 0) {
+    var hand = frame.hands[0];
+    if (hand.confidence > 0.5 && hand.pinchStrength  > 0.6) {
+      var pinchFinger = findPinchFinger(hand);
+      //console.log(pinchFinger);
+    }
+  }
+});
+
+let findPinchFinger = (hand) => {
+  let pincher;
+  let closest = 500;
+  for(let f = 1; f < 5; f++)
+  {
+    let current = hand.fingers[f];
+    let distance = Leap.vec3.distance(hand.thumb.tipPosition, current.tipPosition);
+    if(current != hand.thumb && distance < closest)
+    {
+      closest = distance;
+      pincher = current; 
+    }
+  } 
+
+  return pincher.type - 1;
 }
