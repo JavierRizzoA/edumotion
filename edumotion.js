6d24681f1fd02fd01dd1873c4a75ce034c18d3ce
MA ELIZABETH NAVARRETE S <maelizabethnavarretes@MacBook-Air-de-MA.local>
2015-09-11 22:29:48 -0700

    added serialport function and recognition of Arduino connected to serialport

--- edumotion.js
+++ edumotion.js
@@ -5,14 +5,37 @@ var previousFrame = null;
 // Setup Leap loop with frame callback function
 var controllerOptions = {enableGestures: true};
 
+var SerialPort = require("serialport").SerialPort
+var serialPort;
+SerialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+    serialPort = new SerialPort(port.comName, { baudrate: 57600 });
+    console.log(port.comName);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});
+
+
+
 Leap.loop(controllerOptions, function(frame) {
   if(previousFrame && previousFrame.valid) {
     var rotationAxis = frame.rotationAxis(previousFrame);
     var frameString = "Rotation axis: " + vectorToString(rotationAxis, 2) + "<br />";
     document.getElementById('content').innerHTML = "<div>"+frameString+"</div>";
+  
+    serialPort.write(vectorToArduino(rotationAxis, 3));
+          
   }
   previousFrame = frame;
 });
+
+function vectorToArduino(vector, digits) {
+  if (typeof digits === "undefined") {
+    digits = 1;
+  }
+  return "" + vector[0].toFixed(digits) + " " + vector[2].toFixed(digits);
+}
 
 function vectorToString(vector, digits) {
   if (typeof digits === "undefined") {
